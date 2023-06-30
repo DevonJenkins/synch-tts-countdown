ee152bdb34070ecfdaad6b776233d9804080cc5b
DVNJNKNS <devonmjenkins3@gmail.com>
2023-06-29 23:05:55 -0400

    build out better double feature logic, and implement rollback feature

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const { exec } = require('node:child_process')
 const prompt = require('prompt')
 //----------------imports
 
-//----------------imports
 function main (){
 
 	sonchLevel = 1
 
-	exec("say commence sonch in three [[slnc 1000]] two [[slnc 1000]] one [[slnc 1000]]")
+	//exec("say commence sonch in three [[slnc 1000]] two [[slnc 1000]] one [[slnc 1000]]")
+	console.log("commence sonch")
 
 	recurse(sonchLevel)
 
@@ -17,18 +17,25 @@ function main (){
 		let conf = prompt.get("y/n", function(err, result) {
 
 			result = Object.entries(result)[0][1]
-			if (result != "y") {
+			if (result == "n") {
 				console.log("goodbye =]")
-				console.log(result)
 				prompt.stop()
+			} else if (result == "y") {
+				//set up conditional logic here for 1, 2, and graeater levels
+				if (sonchLevel === 1) {
+					console.log("commense sonch in 3.. 2.. 1..")
+				} else if (sonchLevel === 2) {
+					console.log("commense double feature in 3.. 2.. 1..")
+				} else {
+					console.log(`commense level ${sonchLevel} feature in 3.. 2.. 1..`)	
+				}
 
-			} else {
-
-				console.log(result)
+				//exec(`say commence sonch, feature level ${sonchLevel} in three [[slnc 1000]] two [[slnc 1000]] one [[slnc 1000]]`)
 				sonchLevel++
-				console.log("sonchLevel: ", sonchLevel)
-				exec(`say commence sonch, feature level ${sonchLevel} in three [[slnc 1000]] two [[slnc 1000]] one [[slnc 1000]]
-`)
+				recurse(sonchLevel)
+			} else if (result == "r"){
+				sonchLevel--
+				console.log(sonchLevel)	
 				recurse(sonchLevel)
 			}
 		})
