aeaabd64487cbbc85cd573df7dcff805a0d10ea1
DVNJNKNS <devonmjenkins3@gmail.com>
2023-09-10 10:04:18 -0700

    refactor into more readable/extensible code

--- app.js
+++ app.js
@@ -1,59 +1,41 @@
-//----------------imports
 const { exec } = require("node:child_process");
 const prompt = require("prompt");
-//----------------imports
 
 function main() {
   sonchLevel = 1;
-	message = null
-	//i'd like to make this message more dynamic using template literals
-	//(${level})
-	//then i wouldn't have to keep altering my message. I would ideally just alter
-	//the one message component
+  message = null;
 
-  recurse(sonchLevel, message, 'sonch');
+  recurse(sonchLevel, message, "sonch");
 
   function recurse(sonchLevel, message, level) {
-		//ifsonch level is 1, level == 'sonch'
-		//if sonch level is 2, level == 'double feature'
-		//if sonch level is greater than 2, level == sonchLevel
-		//
-		message =
-    `say commence ${level} in [[pbas 75]] three [[slnc 1000]] [[pbas 50]]two [[slnc 1000]] [[pbas 25]]one [[slnc 1000]]`;
+    message = `say commence ${level} in [[pbas 25]] three [[slnc 1000]] [[pbas 25]]two [[slnc 1000]] [[pbas 25]]one [[slnc 1000]]`;
     exec(message);
-		console.log(message)
+    // console.log(message);
     sonchLevel++;
 
     prompt.start();
+
     let conf = prompt.get("y/n", function (err, result) {
       result = Object.entries(result)[0][1];
-      if (result == "n") {
+      if (result != "n") {
+        if (result == "y") {
+          if (sonchLevel === 1) {
+            level = "sonch";
+          } else if (sonchLevel === 2) {
+            level = "double feature";
+          } else {
+            level = `level ${sonchLevel} feature`;
+          }
+        } else if (result == "r") {
+          sonchLevel--;
+          console.log(sonchLevel);
+        }
+        recurse(sonchLevel, message, level);
+      } else {
         console.log("goodbye =]");
         prompt.stop();
+        //exec("kill %");
         return 1;
-
-      } else if (result == "y") {
-				//if sonch level === 1 , level = 'sonch'
-				//what to do when sonch level == zero?
-					//commence sonch and set level to 2
-				if (sonchLevel === 1) {
-
-
-				} else if (sonchLevel === 2) {
-					recurse(sonchLevel, message, `double feature`);
-					//level = "double feature"
-        } else {
-					recurse(sonchLevel, message, `level ${sonchLevel} feature`);
-					//level = 
-        }
-      } else if (result == "r") {
-				//prompt: what would you like to set the sonch level to?
-					//
-					//
-					sonchLevel--
-				recurse(sonchLevel, message, `level ${sonchLevel} feature`);
-				//level--
-        console.log(sonchLevel);
       }
     });
   }
